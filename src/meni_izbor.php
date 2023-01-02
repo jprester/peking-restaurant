c67a79e5f678c9ef593cd1cebc1b48f26ee5b695
Janko <janko.prester@gmail.com>
2023-01-02 15:43:35 +0100

    switch fully to euros

--- src/meni_izbor.php
+++ src/meni_izbor.php
@@ -100,7 +100,7 @@
 						</div>
 						<div id="jelovnik_bottom"></div>
 						
-						<br /><p class="text-c txt2 italic">Sve cijene su izražene u kunama i u EUR uz konverzijski tečaj 1 eur=7,53450 kn</p><br /><br />
+						<br /><p class="text-c txt2 italic">Sve cijene su izražene u EUR uz konverzijski tečaj 1 eur=7,53450 kn</p><br /><br />
 						
 					  </div>				
 
